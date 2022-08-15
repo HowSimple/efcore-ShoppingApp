661b4bc8e7f60696c28aab1a0a165b4b24cccca0
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-08-15 15:12:28 -0500

    Added initial Migrations setting up DB

--- StoreApp/Migrations/20220814234959_InitialCreate.Designer.cs
+++ StoreApp/Migrations/20220814234959_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StoreApp.DAL;
 
@@ -11,9 +12,10 @@ using StoreApp.DAL;
 namespace StoreApp.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220814234959_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -50,10 +52,6 @@ namespace StoreApp.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ID"), 1L, 1);
-
-                    b.Property<string>("Category")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("DateAdded")
                         .HasColumnType("datetime2");
--- StoreApp/Models/Product.cs
+++ StoreApp/Models/Product.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace WebApplication1.Models
+namespace StoreApp.Models
 {
     public class Product
     {
@@ -8,8 +8,9 @@ namespace WebApplication1.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         [Required]
+        public string Category { get; set; }
         public int QuantityAvailable { get; set; }
-        
-        public System.DateTime DateAdded { get; set; }
+
+        public DateTime DateAdded { get; set; }
     }
 }
